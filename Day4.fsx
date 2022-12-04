63bdf964d693face57833d5fd5122a1744820c40
Kristian Schmidt <kristian@kreutz.us>
2022-12-04 07:31:50 +0100

    Solved day 4

--- Day4.fsx
+++ Day4.fsx
@@ -1,17 +1,31 @@
 #load "Helpers.fsx"
 
 open System
+open Helpers
 
 Environment.CurrentDirectory <- __SOURCE_DIRECTORY__
 
-let data = Helpers.Web.getInput 4
+let data =
+    Helpers.Web.getInput 4
+    |> Array.map (
+        function 
+        | Regex "(\d+)\-(\d+),(\d+)\-(\d+)" [d1;d2;d3;d4] ->
+            ((int d1,int d2),(int d3,int d4)))
 
-let ans1 = data
+let ans1 =
+    data
+    |> Array.filter (fun ((d1,d2),(d3,d4)) -> (d3 >= d1 && d4 <= d2) || (d1 >= d3 && d2 <= d4))
+    |> Array.length
 
 ans1
 
 /// Part 2
 
-let ans2 = data
+let ans2 =
+    data
+    |> Array.map (fun ((d1,d2),(d3,d4)) -> Set.intersect (Set.ofList [d1..d2]) (Set.ofList [d3..d4]))
+    |> Array.filter (Set.isEmpty >> not)
+    |> Array.length
+
 
 ans2
