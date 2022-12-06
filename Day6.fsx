a3ba57e455371ad182cbea1829bca5a2161ad1ea
Kristian Schmidt <kristian@kreutz.us>
2022-12-06 07:14:07 +0100

    Solved day 6

--- Day6.fsx
+++ Day6.fsx
@@ -6,12 +6,36 @@ Environment.CurrentDirectory <- __SOURCE_DIRECTORY__
 
 let data = Helpers.Web.getInput 6
 
-let ans1 = data
+let noDupes [|c1;c2;c3;c4|] =
+    c1 <> c2 && c1 <> c3 && c1 <> c4 &&
+    c2 <> c3 && c2 <> c4 &&
+    c3 <> c4
+
+let startMarker (s : string) =
+    s.ToCharArray()
+    |> Array.windowed 4
+    |> Array.findIndex noDupes
+    |> ((+)4)
+
+startMarker "bvwbjplbgvbhsrlpgdmjqwftvncz"
+
+let ans1 =
+    data
+    |> Array.sumBy startMarker
 
 ans1
 
 /// Part 2
 
-let ans2 = data
+let msgMarker (s : string) =
+    s.ToCharArray()
+    |> Array.windowed 14
+    |> Array.findIndex (fun arr -> let c = Set.ofArray arr |> Set.count
+                                   c = 14)
+    |> ((+)14)
+
+let ans2 =
+    data
+    |> Array.sumBy msgMarker
 
 ans2
