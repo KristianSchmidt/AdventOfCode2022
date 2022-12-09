00653c69b1a83345dafa783723be1603bcd668ff
Kristian Schmidt <kristian@kreutz.us>
2022-12-09 08:47:07 +0100

    Day 9 part 1

--- Day9.fsx
+++ Day9.fsx
@@ -4,7 +4,43 @@ open System
 
 Environment.CurrentDirectory <- __SOURCE_DIRECTORY__
 
-let data = Helpers.Web.getInput 9
+let data =
+    Helpers.Web.getInput 9
+    |> Array.map (Helpers.split " ")
+    |> Array.map (fun [|c;i|] -> char c, int i)
+    |> Array.collect (fun (c,i) -> Array.init i (fun _ -> c))
+
+let moveDir =
+    function
+    | 'L' -> (-1,0)
+    | 'R' -> (1,0)
+    | 'U' -> (0,1)
+    | 'D' -> (0,-1)
+
+let tupAdd (t1x,t1y) (t2x,t2y) = (t1x+t2x,t1y+t2y)
+
+let tailMove (tX,tY) (hX,hY) =
+    let xDiff = abs (hX-tX)
+    let yDiff = abs (hY-tY)
+    if (xDiff > 1 || yDiff > 1) then
+        (sign (hX-tX), sign (hY-tY))
+    else
+        (0,0)
+
+let doMove (tPos, hPos) dir =
+    let newH = tupAdd hPos (moveDir dir)
+    let newT = tupAdd tPos (tailMove tPos newH)
+    //printfn "H: %A T: %A" newH newT
+    (newT,newH)
+
+//let doInstruction (dir,count) tPos hPos =
+//    [1..count] |> List.scan (fun s _ -> doMove s dir) (tPos,hPos)
+
+let res =
+    data |> Array.scan doMove ((0,0),(0,0))
+    |> Array.map fst
+    |> Array.distinct
+    |> Array.length
 
 let ans1 = data
 
